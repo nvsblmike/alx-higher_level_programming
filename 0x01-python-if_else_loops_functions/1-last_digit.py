07cd840f9a62933f135a9a23676c352fa7b96bdd
nvsblmike <soundboylesh77@gmail.com>
2022-06-06 05:54:55 -0700

    edits to 1-last_digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,7 @@ if git > 5:
     print(f"Last digit of {number} is {git} and is greater than 5")
 if git == 0:
     print(f"Last digit of {number} is {git} and is 0")
-if git < 6 and git != 0:
+if git < 6 and git != 0 and number < 0:
     print(f"Last digit of {number} is {sign}{git} and is less than 6 and not 0")
+if git < 6 and git != 0 and number > 0:
+    print(f"Last digit of {number} is {git} and is less than 6 and not 0")
